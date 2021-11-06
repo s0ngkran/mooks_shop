98e0538b6709cb634972da1401a80fcf3ea47f1f
s0ngkran <jinnawat8@gmail.com>
2021-11-06 10:37:10 +0700

    e

--- server/views.py
+++ server/views.py
@@ -791,7 +791,7 @@ class DoUpdate(MyView):
         # do update here
 
         # migrate react
-        a += os.popen('cp update/main.js frontend/static/frontend/main.js')
+        a += os.popen('cp update/main.js frontend/static/frontend/main.js').read()
 
         #############################
         features =[
